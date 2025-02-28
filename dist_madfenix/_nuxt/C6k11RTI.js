6ebc71d2e169cb49a35e6e137c93ed27abb0016d
Valenti Gamez <vgr.gamez@gmail.com>
2025-02-28 18:10:17 +0100

    Correcciones en la lanfing de los dragones custodio.

---
+++ dist_madfenix/_nuxt/C6k11RTI.js
@@ -0,0 +1,1 @@
+import{p as o}from"./D8IS41_H.js";const i=o("/img/dragones-custodio/dragon_fenix.png");export{i as _};
---
+++ dist_madfenix/_nuxt/CayIohOC.js
@@ -0,0 +1,1 @@
+import{V as t,e as a,E as o}from"./D8IS41_H.js";const r=t(e=>{if(!a.get("token").value&&e.path!=="/login")return o("/login")});export{r as default};
