245622c4673966819389f2227edf3f4c28146510
Valenti Gamez <vgr.gamez@gmail.com>
2025-04-02 17:59:23 +0200

    Save progress.

--- layouts/default.vue
+++ layouts/default.vue
@@ -51,6 +51,15 @@
                        v-html="item.text"
             />
 
+            <a href="https://universo.madfenix.com/shelves/nevoran" target="_blank"
+               class="block px-4 py-1 transition duration-200 ease-in-out rounded-full sm:inline-block hover:text-white hover:bg-dark-700">
+              Universo
+            </a>
+            <a href="https://universo.madfenix.com/books/plataforma-web" target="_blank"
+               class="block px-4 py-1 transition duration-200 ease-in-out rounded-full sm:inline-block hover:text-white hover:bg-dark-700">
+              Academia
+            </a>
+
             <a v-if="user.user" @click="logout()"
                class="block px-4 py-1 transition duration-200 ease-in-out rounded-full sm:inline-block hover:text-white hover:bg-dark-700 cursor-pointer"
             >
@@ -116,7 +125,7 @@
                 class="fixed bottom-0 left-0 w-full flex items-center justify-center bg-gradient-to-tr" style="z-index: 100;"
             >
               <div
-                  class="flex grid grid-cols-4 items-center w-full bg-madfenix-gris mx-auto text-xl"
+                  class="flex grid grid-cols-6 items-center w-full bg-madfenix-gris mx-auto text-xl"
               >
                 <nuxt-link to="/"
                            class="block p-4 font-medium rounded-full text-white hover:text-white hover:bg-dark-700 sm:inline-block"
@@ -138,10 +147,27 @@
                   <svg v-else-if="item.text == 'Tienda'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-auto">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
                   </svg>
+
                   <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-auto">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                   </svg>
                 </nuxt-link>
+
+                <a href="https://universo.madfenix.com/shelves/nevoran" target="_blank"
+                   class="block border-l-2 border-dark-600 p-4 font-medium text-white hover:text-white hover:bg-dark-700 sm:inline-block"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-auto">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                  </svg>
+                </a>
+
+                <a href="https://universo.madfenix.com/books/plataforma-web" target="_blank"
+                   class="block border-l-2 border-dark-600 p-4 font-medium text-white hover:text-white hover:bg-dark-700 sm:inline-block"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-auto">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
@@ -188,6 +214,16 @@
             </li>
             <li v-if="user.user" v-for="(item, key) in items" :key="key" class="font-medium text-white hover:text-white">
               <nuxt-link :to="item.path" v-html="item.text" />
+            </li>
+            <li class="font-medium text-white hover:text-white">
+              <a href="https://universo.madfenix.com/shelves/nevoran" target="_blank" class="cursor-pointer" >
+                Universo
+              </a>
+            </li>
+            <li class="font-medium text-white hover:text-white">
+              <a href="https://universo.madfenix.com/books/plataforma-web" target="_blank" class="cursor-pointer" >
+                Academia
+              </a>
             </li>
             <li v-if="user.user" class="font-medium text-white hover:text-white">
               <a @click="logout()" class="cursor-pointer" >
